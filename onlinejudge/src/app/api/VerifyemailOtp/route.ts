c06144373abbc2ec2d1b7d429d6e16455fbc3b8c
RaviKant <rk3727000@gmail.com>
2024-06-18 13:08:33 +0545

    Adding email verification and unique username

---
+++ onlinejudge/src/app/api/VerifyemailOtp/route.ts
@@ -0,0 +1,57 @@
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+export async function POST(request:Request){
+    await dbConnect()
+
+    try {
+        const {username, code} = await request.json()
+
+        const decodedUsername = decodeURIComponent(username)
+        
+        const user = await UserModel.findOne({
+            username:decodedUsername
+        })
+
+
+        if(!user){
+            return Response.json({
+                success:false,
+                message:"User not found"
+            },{ status: 500})
+        }
+
+
+        const isOtpValid = user.verifyCode === code
+        const isCodeExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+
+        if(isOtpValid && isCodeExpired) {
+            user.isVerified = true;
+            await user.save()
+
+            return Response.json({
+                success:true,
+                message:"Account verified successfully"
+            },{ status: 200})
+        }else if(!isCodeExpired){
+            return Response.json({
+                success:false,
+                message:"Verification code is expired please signUp again to get new Otp"
+            },{ status: 400})
+        }else{
+            return Response.json({
+                success:false,
+                message:"Invalid verfication otp"
+            },{ status: 400})
+        }
+
+        
+    } catch (error) {
+        console.error("Error verifying email otp" , error);
+        return Response.json({
+            success:false,
+            message:"Error verifying email otp"
+        },{ status: 500})
+    }
+}
